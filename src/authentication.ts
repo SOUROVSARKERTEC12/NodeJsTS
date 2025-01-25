7b287a2fc8935118d5f36eb9f0af92dfb94ab547
SOUROV <sourovsarker005@gmail.com>
2025-01-25 23:47:03 +0600

    dependency injection pattern

---
+++ src/authentication.ts
@@ -0,0 +1,9 @@
+import express from "express";
+//import {register} from '../controller/authentication';
+
+export default (router: express.Router) => {
+  //router.post('/register', register);
+};
+
+
+// This style is commonly referred to as the router module pattern or dependency injection pattern for routing.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import { log } from "console";
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  credentials: true
+}));
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response):any => {
   let x = 10;
